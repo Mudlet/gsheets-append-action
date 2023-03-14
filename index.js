953004130d96dfba09aa04810a2d60181508d745
keneanung <keneanung@gmail.com>
2023-03-14 21:52:00 +0100

    First implementation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import { info, getInput } from "@actions/core";
 import { JWT } from "google-auth-library";
 import { google } from "googleapis";
+import { readFileSync } from "fs";
 const sheets = google.sheets("v4");
 
 const user = getInput("GOOGLE_USER_MAIL", {
@@ -18,11 +19,22 @@ const spreadsheetId = getInput("SPREADSHEET", {
   required: true,
 });
 const tableStartCell = getInput("TABLE_START_CELL") || "A1";
-const dataEnvironmentVariableName = getInput("DATA_ENV_NAME", {
-  required: true,
-});
+const dataEnvironmentVariableName = getInput("DATA_ENV_NAME");
+const dataFileName = getInput('DATA_FILE_PATH');
 
-const data = process.env[dataEnvironmentVariableName]
+if(!dataEnvironmentVariableName && ! dataFileName){
+  throw new Error("Either 'DATA_ENV_NAME' or 'DATA_FILE_PATH' are required.")
+}
+
+let rawData;
+
+if(dataEnvironmentVariableName){
+  rawData = readFileSync(dataEnvironmentVariableName, { encoding: 'utf-8'})
+} else {
+  rawData = process.env[dataEnvironmentVariableName]
+}
+
+const data = rawData
   .split("\n")
   .map((dataRow) => dataRow.split(","));
 
